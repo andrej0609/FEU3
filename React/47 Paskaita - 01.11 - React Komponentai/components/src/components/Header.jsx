859990db73d863169cf190bf7aa7a75ed2e4974d
andrej0609 <andrejus.galkinas@gmail.com>
2023-01-11 15:13:07 +0200

    pakeitimas

--- React/47 Paskaita - 01.11 - React Komponentai/components/src/components/Header.jsx
+++ React/47 Paskaita - 01.11 - React Komponentai/components/src/components/Header.jsx
@@ -2,9 +2,10 @@ const Header = () => {
   return (
     <>
       <div className="header">
-      <img src="https://asiamountains.net/upload/slide/slide-1960x857-07.jpg" alt="" />
+        <img src="https://asiamountains.net/upload/slide/slide-1960x857-07.jpg" alt="" />
         <h1>Title</h1>
-        <p>Some about text <br /> in two lines</p>
+        <p>Some about text</p>
+        <p>in two lines</p>
         <button>Button</button>
       </div>
     </>
