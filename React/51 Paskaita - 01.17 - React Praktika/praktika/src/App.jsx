c4202e927c791415837f3d27f82083acacc59387
andrej0609 <andrejus.galkinas@gmail.com>
2023-01-19 09:33:46 +0200

    change

--- React/51 Paskaita - 01.17 - React Praktika/praktika/src/App.jsx
+++ React/51 Paskaita - 01.17 - React Praktika/praktika/src/App.jsx
@@ -1,16 +1,41 @@
 import './App.css';
 import Header from './komponentai/Header';
 import Forma from './komponentai/Forma';
-import Data from './komponentai/Data';
 import Footer from './komponentai/Footer';
+import React, { useState, useEffect } from 'react';
+import Kalnas from './komponentai/Kalnas';
+
 
 const App = () => {
+  const [kalnas, setKalnas] = useState(null);
+
+  const fetchData = async () => {
+    const fetchinamiDuomenys = await fetch('http://localhost:5000/kalnai')
+      .then(res => res.json());
+    console.log(fetchinamiDuomenys);
+    setKalnas(fetchinamiDuomenys);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <>
       <Header />
       <Forma />
-      <Data />
+      <div className='korteles'>
+        {
+          kalnas ?
+            kalnas.map((kalnas) =>
+              <Kalnas
+                key={kalnas.id}
+                kalnas={kalnas}
+              />
+            ) :
+            <img src="https://media.tenor.com/On7kvXhzml4AAAAj/loading-gif.gif" alt="loading" />
+        }
+      </div>
       <Footer />
     </>
   );
